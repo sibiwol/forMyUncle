e43b784821bf383cc6f967411fa3885bfd8f2247
sibiwol <thinkerring2020@gmail.com>
2022-05-07 22:57:19 +0900

    api 가져옴

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let xhr = new XMLHttpRequest();
+let url =
+  "http://openapi.seoul.go.kr:8088/684d77555a797539353654426b5366/xml/bigKcyPlatPlcInfo/1/5/"; /*URL*/
+xhr.open("GET", url);
+xhr.onreadystatechange = function () {
+  if (this.readyState === xhr.DONE) {
+    // <== 정상적으로 준비되었을때
+    if (xhr.status === 200 || xhr.status === 201) {
+      // <== 호출 상태가 정상적일때
+      alert(
+        "Status: " +
+          this.status +
+          "\nHeaders: " +
+          JSON.stringify(this.getAllResponseHeaders()) +
+          "\nBody: " +
+          this.responseText
+      );
+    }
+  }
+};
+xhr.send("");
+
+for ( i = 0; i)
